fb390d44001f1a86fed93290b68fd714bb627884
Beatriz Cristina Marques <beatrizmaarqueszo@gmail.com>
2022-10-06 13:03:55 -0300

    Adicionando paginação

--- clientes/views.py
+++ clientes/views.py
@@ -1,11 +1,16 @@
 # clientes/views.py
-from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, viewsets
 
 from clientes.models import Cliente
 from clientes.serializers import ClienteSerializer
 
 
 class ClientesViewSet(viewsets.ModelViewSet):
-    """Listando clientes"""
+    """Listando todos os clientes"""
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
+    ordering_fields = ['nome']
+    search_fields = ['nome','cpf']
+    filterset_fields = ['ativo']
